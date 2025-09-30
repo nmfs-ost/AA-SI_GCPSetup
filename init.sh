4c44ef676673f262210938844212e4056efdb04a
Michael C Ryan - NOAA <160658063+mryan11@users.noreply.github.com>
2025-09-30 08:58:35 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,13 @@ echo "🏚️ Preparing workstation setup at $HOME..."
 mkdir -p "$HOME"
 echo "🛠️ Base station ($HOME) is operational."
 
+
+sudo rm /etc/apt/sources.list.d/helm-stable-debian.list
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+
+
 sudo apt update
 sudo apt upgrade -y
 
